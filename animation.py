6dfeb78245ef8faaf8aeb868dec441f5de0b72f2
Miroslav Savel <miroslav.savel@gmail.com>
2022-04-12 14:59:08 +0200

    Fast animation, loop through images

--- animation.py
+++ animation.py
@@ -1,12 +1,32 @@
 import pygame, sys
 
+# https://www.youtube.com/watch?v=MYaxPa_eZS0&list=PL8ui5HK3oSiHnIdi0XIAVXHAeulNmBrLy&index=3
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
-        self.image = pygame.Surface([20,20])
-        self.image.fill((255,255,255))
+        self.sprites = [] # here we load the images
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_1.png"))
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_2.png"))
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_3.png"))
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_4.png"))
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_5.png"))
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_6.png"))
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_7.png"))
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_8.png"))
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_9.png"))
+        self.sprites.append(pygame.image.load("./resources/images/animation/attack_10.png"))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
         self.rect = self.image.get_rect()
-        self.rect.topleft = [pos_x, pos_y]
+        self.rect.topleft = [pos_x,pos_y]
+
+    def update(self):
+        self.current_sprite += 1
+        if self.current_sprite >= len(self.sprites):
+            self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
 
 #Pygame setup
 pygame.init()
@@ -18,7 +38,7 @@ clock = pygame.time.Clock()
 
 # Creating the sprites and groups
 moving_sprites = pygame.sprite.Group()
-player = Player(100, 100)
+player = Player(10, 10)
 moving_sprites.add(player)
 while True:
     # event handler
@@ -26,8 +46,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    #drwing
+    #drawing
     screen.fill('black')
     moving_sprites.draw(screen)
+    moving_sprites.update() # this will call update method on every sprite in the group
     pygame.display.flip()
     clock.tick(60)
